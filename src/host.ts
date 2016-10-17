f2c774180b4e02e161bc0e3d55c52559e1e2e363
Joe Larson <larson.joseph@gmail.com>
2016-10-17 15:00:30 -0600

    adds more top leve exports

--- src/host.ts
+++ src/host.ts
@@ -4,6 +4,8 @@ import Host from './core/bitwig/Host';
 
 // TODO: this shouldn't need to be cast as api.Host
 let host: api.Host = new Host(global.host) as any as api.Host;
+// swap global scope host for proxy host (but don't use it anyways)
+global.host = host;
 
 
 export default host;
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@ import config from './config';
 import host from './host';
 import session from './session';
 import * as utils from './utils';
-import { View } from './core/view';
+import { View, ViewCollection } from './core/view';
+import { AbstractDevice, DeviceTemplate, DeviceControl, DeviceControlCollection } from './core/device';
+import { AbstractControl, Button, ControlSet, Knob } from './core/control';
 
 
-export {
-    config,
-    host,
-    session,
-    utils,
-    View,
-};
+export { config, host, session, utils };
+export { AbstractDevice, DeviceTemplate, DeviceControl, DeviceControlCollection };
+export { View, ViewCollection };
+export { AbstractControl, Button, ControlSet, Knob };
