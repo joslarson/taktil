e3b5900b75e34ab18e4ea30a346df89c78249efd
Joe Larson <larson.joseph@gmail.com>
2017-02-26 23:31:50 -0700

    continues work on test project to catch framework wholes

---
+++ test-project/src/components/SceneButtonBank.ts
@@ -0,0 +1,53 @@
+import { AbstractComponentSet, AbstractButton, SimpleControl, session } from 'typewig';
+
+import bitwig from 'apistore';
+
+
+export abstract class AbstractSceneButton extends AbstractButton {
+    abstract index: number;
+    scene: API.Scene;
+    state = { ...this.state, exists: false, color: { r: 1, g: 0, b: 1 } };
+
+    renderControl(control: SimpleControl) {
+        control.render({
+            value: this.state.on ? control.resolution - 1 : 0,
+            disabled: !this.state.exists,
+            // color: this.state.color,
+        });
+    }
+
+    onRegister() {
+        this.scene = bitwig.sceneBank.getScene(this.index);
+
+        this.scene.addIsSelectedInEditorObserver(isSelected => {
+            this.setState({ ...this.state, on: isSelected })
+        });
+
+        this.scene.exists().addValueObserver(sceneExists => {
+            this.setState({ ...this.state, exists: sceneExists });
+        });
+    }
+
+    onPress() {
+        if (!session.modeIsActive('SELECT')) {
+            if (!this.scene.exists().get()) {
+                for (let i = 0; i <= this.index; i++) {
+                    if (!bitwig.sceneBank.getScene(i).exists().get()) {
+                        bitwig.createScene.invoke();
+                    }
+                }
+            } else {
+                this.scene.launch();
+            }
+        }
+        this.scene.selectInEditor();
+    }
+}
+
+export default class SceneButtonBank extends AbstractComponentSet {
+    getComponentClass(index: number) {
+        return class SceneButton extends AbstractSceneButton {
+            index = index;
+        }
+    }
+}
---
+++ test-project/src/components/browser.ts
@@ -0,0 +1,27 @@
+import { AbstractButton } from 'typewig';
+
+import bitwig from 'apistore';
+
+
+export class ToggleBrowser extends AbstractButton {
+    onRegister() {
+        bitwig.popupBrowser.exists().addValueObserver(browserExists => {
+            this.setState({ ...this.state, on: browserExists });
+        });
+    }
+
+    onPress() {
+        if (bitwig.popupBrowser.exists().get()) {
+            bitwig.popupBrowser.cancel();
+        } else {
+            bitwig.cursorTrack.browseToInsertAtEndOfChain();
+        }
+    }
+}
+
+
+export class BrowserExitButton extends AbstractButton {
+    onPress() {
+        if (bitwig.popupBrowser.exists().get()) bitwig.popupBrowser.cancel();
+    }
+}
