88cbab1ced623324d1d1a974a4834c1fdf1b516c
Joe Larson <larson.joseph@gmail.com>
2017-05-05 21:29:22 -0600

    Make private and protected methods on built in component classes public

--- src/core/component/AbstractButton.ts
+++ src/core/component/AbstractButton.ts
@@ -39,42 +39,42 @@ abstract class AbstractButton<
     onDoubleRelease?(): void;
 
     onControlInput(control: AbstractControl, input: AbstractControlBaseState) {
-        if (this.onPress) this._handlePress(input.value);
-        if (this.onLongPress) this._handleLongPress(input.value);
-        if (this.onDoublePress) this._handleDoublePress(input.value);
-        if (this.onRelease) this._handleRelease(input.value);
-        if (this.onDoubleRelease) this._handleDoubleRelease(input.value);
+        if (this.onPress) this.handlePress(input.value);
+        if (this.onLongPress) this.handleLongPress(input.value);
+        if (this.onDoublePress) this.handleDoublePress(input.value);
+        if (this.onRelease) this.handleRelease(input.value);
+        if (this.onDoubleRelease) this.handleDoubleRelease(input.value);
     }
 
-    protected _isPress(value: number) {
+    isPress(value: number) {
         return value > 0;
     }
 
-    protected _isRelease(value: number) {
+    isRelease(value: number) {
         return value === 0;
     }
 
-    protected _isDoublePress(value: number) {
-        return this.memory['doublePress'] && this._isPress(value);
+    isDoublePress(value: number) {
+        return this.memory['doublePress'] && this.isPress(value);
     }
 
-    protected _isDoubleRelease(value: number) {
-        return this.memory['doubleRelease'] && this._isRelease(value);
+    isDoubleRelease(value: number) {
+        return this.memory['doubleRelease'] && this.isRelease(value);
     }
 
-    private _handlePress(value: number) {
+    handlePress(value: number) {
         // if it's not a press, not implemented or is a doublePress, ignore it
-        if (!this._isPress(value) || this.memory['doublePress']) return;
+        if (!this.isPress(value) || this.memory['doublePress']) return;
         // handle single press
         if (this.onPress) this.onPress();
     }
 
-    private _handleDoublePress(value: number) {
+    handleDoublePress(value: number) {
         // if it's not a press or not implemented, ignore it
-        if (!this._isPress(value)) return;
+        if (!this.isPress(value)) return;
 
         // if is doublePress
-        if (this._isDoublePress(value)) {
+        if (this.isDoublePress(value)) {
             if (this.onDoublePress) this.onDoublePress();
         } else {
             // setup interval task to remove self after DOUBLE_PRESS_DURATION
@@ -84,12 +84,12 @@ abstract class AbstractButton<
         }
     }
 
-    private _handleLongPress(value: number) {
+    handleLongPress(value: number) {
         // if it's a doublePress or is not implemented, ignore it
-        if (this._isDoublePress(value)) return;
+        if (this.isDoublePress(value)) return;
 
         // if it's a press schedule the callback
-        if (this._isPress(value)) {
+        if (this.isPress(value)) {
             // schedule callback
             this.memory['longPress'] = setTimeout(() => {
                 if (this.onLongPress) this.onLongPress();
@@ -103,19 +103,19 @@ abstract class AbstractButton<
         }
     }
 
-    private _handleRelease(value: number) {
+    handleRelease(value: number) {
         // if it's not a release, not implemented or is a doubleRelease, ignore it
-        if (!this._isRelease(value) || this.memory['doubleRelease']) return;
+        if (!this.isRelease(value) || this.memory['doubleRelease']) return;
         // handle single release
         if (this.onRelease) this.onRelease();
     }
 
-    private _handleDoubleRelease(value: number) {
+    handleDoubleRelease(value: number) {
         // if it's not a release or not implemented, ignore it
-        if (!this._isRelease(value)) return;
+        if (!this.isRelease(value)) return;
 
         // if is doubleRelease
-        if (this._isDoubleRelease(value) && this.onDoubleRelease) {
+        if (this.isDoubleRelease(value) && this.onDoubleRelease) {
             this.onDoubleRelease();
         } else {
             // setup timeout task to remove self after this.DOUBLE_PRESS_DELAY
