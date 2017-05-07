b2e8621a6343a105332496f799f39e0f59651803
Joe Larson <larson.joseph@gmail.com>
2017-05-07 17:27:08 -0600

    clarify test message

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -4,7 +4,7 @@ import { midiMessageToHex } from './utils';
 
 
 describe('midiMessageToHex', () => {
-    it('should return midi message hex representation string', () => {
+    it('should return correct midi message hex representation string', () => {
         expect(midiMessageToHex({ status: 0xB0, data1: 0xA4, data2: 0x7F })).to.equal('B0A47F');
     });
 });
