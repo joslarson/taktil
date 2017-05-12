1535cf97a07fbb7845ce0c070e549b503c27bc45
Joe Larson <larson.joseph@gmail.com>
2017-05-11 23:56:14 -0600

    Remove globals

--- src/env.ts
+++ src/env.ts
@@ -5,79 +5,80 @@ import Logger from './core/Logger';
 (() => {
     const global = Function('return this')() || (42, eval)('this');
 
-    loadAPI(2); // load bitwig api v2
+    if (global.loadAPI) {
+        loadAPI(2); // load bitwig api v2
 
-    // add global reference to global namespace
-    global.global = global;
+        // allows us to provide better error messages and to catch errors early
+        // when code that is only allowed to run inside init is place outside.
+        global.__is_init__ = false;
 
-    // connect logger as global console
-    global.console = new Logger();
+        // add global reference to global namespace
+        global.global = global;
 
-    // hookup dummy function to unsupported logger methods
+        // connect logger as global console
+        global.console = new Logger();
 
-    // Console-polyfill. MIT license.
-    // https://github.com/paulmillr/console-polyfill
-    // Make it safe to do console.log() always.
-    var con = global.console;
-    var prop, method;
-    var dummy = function() {};
-    var properties = ['memory'];
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group',
-        'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profiles',
-        'profileEnd', 'show', 'table', 'time', 'timeEnd', 'timeline', 'timelineEnd', 'timeStamp',
-        'trace', 'warn',
-    ];
-    while (prop = properties.pop()) if (!con[prop]) con[prop] = {};
-    while (method = methods.pop()) if (typeof con[method] !== 'function') con[method] = dummy;
+        // hookup dummy function to unsupported logger methods
 
-
-    // allows us to provide better error messages and to catch errors early
-    // when code that is only allowed to run inside init is place outside.
-    global.__is_init__ = false;
+        // Console-polyfill. MIT license.
+        // https://github.com/paulmillr/console-polyfill
+        // Make it safe to do console.log() always.
+        var con = global.console;
+        var prop, method;
+        var dummy = function() {};
+        var properties = ['memory'];
+        var methods = [
+            'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group',
+            'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profiles',
+            'profileEnd', 'show', 'table', 'time', 'timeEnd', 'timeline', 'timelineEnd', 'timeStamp',
+            'trace', 'warn',
+        ];
+        while (prop = properties.pop()) if (!con[prop]) con[prop] = {};
+        while (method = methods.pop()) if (typeof con[method] !== 'function') con[method] = dummy;
 
 
-    class DelayedTask {
-        callback: Function;
-        delay: number;
-        repeat: boolean;
-        cancelled = false;
+        class DelayedTask {
+            callback: Function;
+            delay: number;
+            repeat: boolean;
+            cancelled = false;
 
-        constructor (callback: (...args: any[]) => any, delay = 0, repeat = false) {
-            this.callback = callback;
-            this.delay = delay;
-            this.repeat = repeat;
+            constructor (callback: (...args: any[]) => any, delay = 0, repeat = false) {
+                this.callback = callback;
+                this.delay = delay;
+                this.repeat = repeat;
+            }
+
+            start(...args: any[]) {
+                host.scheduleTask(() => {
+                    if (!this.cancelled) {
+                        this.callback.call(args);
+                        if (this.repeat) this.start(...args);
+                    }
+                }, this.delay);
+                return this;
+            }
+
+            cancel() {
+                this.cancelled = true;
+                return this;
+            }
         }
 
-        start(...args: any[]) {
-            host.scheduleTask(() => {
-                if (!this.cancelled) {
-                    this.callback.call(args);
-                    if (this.repeat) this.start(...args);
-                }
-            }, this.delay);
-            return this;
+        global.setTimeout = function setTimeout(callback: (...args: any[]) => any, delay = 0, ...params: any[]) {
+            return new DelayedTask(callback, delay).start(...params);
         }
 
-        cancel() {
-            this.cancelled = true;
-            return this;
+        global.clearTimeout = function clearTimeout(timeout: DelayedTask) {
+            timeout.cancel();
+        }
+
+        global.setInterval = function setInterval(callback: (...args: any[]) => any, delay = 0, ...params: any[]) {
+            return new DelayedTask(callback, delay, true).start(...params);
+        }
+
+        global.clearInterval = function clearInterval(interval: DelayedTask) {
+            interval.cancel();
         }
     }
-
-    global.setTimeout = function setTimeout(callback: (...args: any[]) => any, delay = 0, ...params: any[]) {
-        return new DelayedTask(callback, delay).start(...params);
-    }
-
-    global.clearTimeout = function clearTimeout(timeout: DelayedTask) {
-        timeout.cancel();
-    }
-
-    global.setInterval = function setInterval(callback: (...args: any[]) => any, delay = 0, ...params: any[]) {
-    return new DelayedTask(callback, delay, true).start(...params);
-}
-
-global.clearInterval = function clearInterval(interval: DelayedTask) {
-    interval.cancel();
-}
 })();
