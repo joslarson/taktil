dacfb7fa2e7214e666025a322607fb16a3e07438
Joe Larson <larson.joseph@gmail.com>
2017-08-27 17:28:10 -0600

    look into possibility of single library export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,62 @@
+import { Control, ControlState } from './core/control';
+import { View } from './core/view';
+import { Session } from './core/session';
+
+export const session = new Session();
+
+export function registerControls(controls: { [name: string]: Control }) {
+    session.registerControls(controls);
+}
+
+export function getControls() {
+    return session.controls;
+}
+
+export function registerViews(views: { [name: string]: typeof View }) {
+    return session.registerViews(views);
+}
+
+export function getViews() {
+    return session.views;
+}
+
+export function getActiveView() {
+    return session.activeView;
+}
+
+export function activateView(view: string) {
+    return session.activateView(view);
+}
+
+export function getActiveModes() {
+    return session.activeModes;
+}
+
+export function activateMode(mode: string) {
+    return session.activateMode(mode);
+}
+
+export function deactivateMode(mode: string) {
+    return session.deactivateMode(mode);
+}
+
+export function modeIsActive(mode: string) {
+    return session.modeIsActive(mode);
+}
+
+export function on(
+    label: 'activateMode' | 'deactivateMode',
+    callback: (mode: string) => void
+): void;
+export function on(label: 'activateView', callback: (view: typeof View) => void): void;
+export function on(
+    label: 'init' | 'registerControls' | 'registerViews' | 'flush' | 'exit',
+    callback: () => void
+): void;
+export function on(label: any, callback: (...args: any[]) => any) {
+    session.on(label, callback);
+}
+
 export * from './core/component';
 export * from './core/control';
 export * from './core/helpers';
