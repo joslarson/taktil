f40c3bfeea3a1c1daa419ecc9b87fd774cb94ffb
Joseph Larson <larson.joseph@gmail.com>
2023-03-16 00:20:02 -0600

    Update useButton hook; re-export from root

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,63 +1,78 @@
 import React from 'react';
 
-export const useButtonCallbacks = (
-  isPressed: boolean,
-  {
-    onPress,
-    onLongPress,
-    onDoublePress,
-    onRelease,
-    onDoubleRelease,
-  }: {
-    onPress?: () => void;
-    onLongPress?: () => void;
-    onDoublePress?: () => void;
-    onRelease?: () => void;
-    onDoubleRelease?: () => void;
-  }
-) => {
+export const useButton = ({
+  onPress,
+  onLongPress,
+  onDoublePress,
+  onRelease,
+  onDoubleRelease,
+}: {
+  onPress?: () => void;
+  onLongPress?: () => void;
+  onDoublePress?: () => void;
+  onRelease?: () => void;
+  onDoubleRelease?: () => void;
+}) => {
   const timeoutsRef = React.useRef({
     longPress: 0,
     recentPress: 0,
     recentRelease: 0,
   });
-  const [recentlyPressed, setRecentlyPressed] = React.useState(false);
-  const [recentlyReleased, setRecentlyReleased] = React.useState(false);
-  const [hasBeenPressed, setHasBeenPressed] = React.useState(false);
 
   React.useEffect(() => {
-    isPressed
-      ? recentlyPressed
-        ? onDoublePress
-          ? onDoublePress()
-          : onPress && onPress()
-        : onPress && onPress()
-      : recentlyReleased
-      ? onDoubleRelease
-        ? onDoubleRelease()
-        : onRelease && onRelease()
-      : hasBeenPressed && onRelease && onRelease();
-
-    clearTimeout(timeoutsRef.current.longPress);
-
-    if (isPressed) {
-      if (!hasBeenPressed) setHasBeenPressed(true);
-
-      timeoutsRef.current.longPress = setTimeout(() => {
-        onLongPress && onLongPress();
-      }, 500);
-
-      setRecentlyPressed(true);
+    return () => {
+      // cancel timeouts on unmount
+      clearTimeout(timeoutsRef.current.longPress);
       clearTimeout(timeoutsRef.current.recentPress);
-      timeoutsRef.current.recentPress = setTimeout(() => {
-        setRecentlyPressed(false);
-      }, 250);
-    } else {
-      setRecentlyReleased(true);
       clearTimeout(timeoutsRef.current.recentRelease);
-      timeoutsRef.current.recentRelease = setTimeout(() => {
-        setRecentlyReleased(false);
-      }, 250);
-    }
-  }, [isPressed]);
+    };
+  }, []);
+
+  const recentlyPressedRef = React.useRef(false);
+  const recentlyReleasedRef = React.useRef(false);
+  const hasBeenPressedRef = React.useRef(false);
+
+  const fireButtonEvent = React.useCallback(
+    (event: 'pressed' | 'released') => {
+      event === 'pressed'
+        ? recentlyPressedRef.current
+          ? onDoublePress
+            ? onDoublePress()
+            : onPress && onPress()
+          : onPress && onPress()
+        : recentlyReleasedRef.current
+        ? onDoubleRelease
+          ? onDoubleRelease()
+          : onRelease && onRelease()
+        : hasBeenPressedRef.current && onRelease && onRelease();
+
+      // if there's a pending longPress timeout, press or release should cancel that
+      clearTimeout(timeoutsRef.current.longPress);
+
+      if (event === 'pressed') {
+        if (!hasBeenPressedRef.current) hasBeenPressedRef.current = true;
+
+        if (onLongPress) {
+          timeoutsRef.current.longPress = setTimeout(onLongPress, 500);
+        }
+
+        // set recently pressed & schedule reset
+        clearTimeout(timeoutsRef.current.recentPress);
+        recentlyPressedRef.current = true;
+        timeoutsRef.current.recentPress = setTimeout(() => {
+          recentlyPressedRef.current = false;
+        }, 250);
+      } else {
+        // set recently released & schedule reset
+        clearTimeout(timeoutsRef.current.recentRelease);
+        recentlyReleasedRef.current = true;
+        timeoutsRef.current.recentRelease = setTimeout(() => {
+          recentlyReleasedRef.current = false;
+        }, 250);
+      }
+    },
+    [onPress, onLongPress, onDoublePress, onRelease, onDoubleRelease]
+  );
+
+  return fireButtonEvent;
 };
